b5fe85a468bc88666e66c8679536ba991d857d1f
Erhard Nagy <Erson1989x@gmail.com>
2025-01-30 19:53:57 +0200

    test

---
+++ src/redux/store.js
@@ -0,0 +1,67 @@
+import { createStore } from "redux";
+
+const initialState = {
+  balance: 0,
+  loan: 0,
+  loanPurpose: "",
+};
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case "account/deposit":
+      return {
+        ...state,
+        balance: state.balance + action.payload,
+      };
+    case "account/withdrawal":
+      return {
+        ...state,
+        balance: state.balance - action.payload,
+      };
+    case "account/requestLoan":
+      if (state.loan > 0) return state;
+      //LAter
+      return {
+        ...state,
+        loan: action.payload.amount,
+        loanPurpose: action.payload.purpose,
+        balance: state.balance + action.payload.amount
+      };
+    case "account/payLoan":
+      return {
+        ...state,
+        loan: 0,
+        loanPurpose: "",
+        balance: state.balance - state.loan,
+      };
+
+    default:
+      return state;
+  }
+};
+
+const store = createStore(reducer);
+
+store.dispatch({
+  type: "account/deposit",
+  payload: 500,
+});
+
+store.dispatch({
+  type: "account/withdrawal",
+  payload: 200,
+});
+
+store.dispatch({
+  type: "account/requestLoan",
+  payload: {
+    amount: 1000,
+    purpose: "Buy a car",
+  },
+});
+
+store.dispatch({
+  type: "account/payLoan",
+});
+
+console.log(store.getState());
