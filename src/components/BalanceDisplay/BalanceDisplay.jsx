76e76355ee3a257c32c2a956565d3029c613fb6c
Erhard Nagy <Erson1989x@gmail.com>
2025-01-31 00:05:01 +0200

    test

--- src/components/BalanceDisplay/BalanceDisplay.jsx
+++ src/components/BalanceDisplay/BalanceDisplay.jsx
@@ -1,3 +1,5 @@
+import { connect } from "react-redux";
+
 function formatCurrency(value) {
   return new Intl.NumberFormat("en", {
     style: "currency",
@@ -5,8 +7,14 @@ function formatCurrency(value) {
   }).format(value);
 }
 
-function BalanceDisplay() {
-  return <div className="balance">{formatCurrency(123456)}</div>;
+const BalanceDisplay = ( { balance } ) => {
+  return <div className="balance">{formatCurrency(balance)}</div>;
 }
 
-export default BalanceDisplay;
+const mapStateToProps = (state) => {
+  return {
+    balance: state.account.balance,
+  };
+}; 
+
+export default connect( mapStateToProps )(BalanceDisplay)
